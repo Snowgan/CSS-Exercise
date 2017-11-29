75ba5934ca7de009d1462e8496eec2f706960219
Jennifer GAN <jennifergxl@gmail.com>
2017-11-29 23:37:48 +0800

    Add gulp connect as webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const connect = require('gulp-connect');
+
+gulp.task('server', function () {
+  connect.server({
+    livereload: true
+  });
+})
 
 gulp.task('sass', function () {
   gulp.src('sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./css/'));
+      .pipe(gulp.dest('./css/'))
+      .pipe(connect.reload());
 });
 
-gulp.task('default', function () {
+gulp.task('html', function () {
+  gulp.src('./*.html')
+      .pipe(connect.reload());
+})
+
+gulp.task('watch', function () {
   gulp.watch('sass/**/*.scss', ['sass']);
-});
+  gulp.watch('./*.html', ['html']);
+})
+
+gulp.task('default', ['sass', 'server', 'watch']);
